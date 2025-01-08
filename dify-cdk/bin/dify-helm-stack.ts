66d2f2632f39a6c72d89fe6cfb7aaca2a0a79558
Kevin Zhao <kevin8093@126.com>
2025-01-08 12:42:44 +0800

    update

--- dify-cdk/bin/dify-helm-stack.ts
+++ dify-cdk/bin/dify-helm-stack.ts
@@ -126,7 +126,7 @@ export class DifyHelmStack extends cdk.Stack {
           host: '',
           port: '',
           enableTLS: false,
-          image: { tag: '0.14.2' },
+          image: { tag: '0.15.0' },
           edition: 'SELF_HOSTED',
           storageType: 's3',
           extraEnvs: [],
